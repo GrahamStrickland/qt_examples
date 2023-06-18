8d7e86e5297bf07f6eee374800054b552f043423
Graham Strickland <grahamhstrickland@gmail.com>
2023-06-18 13:14:57 +0200

    Add timer to Readonly Table example

--- readonly_table/src/mymodel.h
+++ readonly_table/src/mymodel.h
@@ -1,4 +1,6 @@
 #include <QAbstractTableModel>
+#include <QTime>
+#include <QTimer>
 
 class MyModel : public QAbstractTableModel
 {
@@ -9,4 +11,10 @@ public:
     int rowCount(const QModelIndex &parent = QModelIndex()) const override;
     int columnCount(const QModelIndex &parent = QModelIndex()) const override;
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+
+public slots:
+    void timerHit();
+
+private:
+    QTimer *timer;
 };
